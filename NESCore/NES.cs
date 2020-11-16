258d6f7a282a722ca7d7b3524c7ffec57d5b1cf6
Jose <josementi@gmail.com>
2020-11-16 00:11:48 +0000

    Finish speed matching stuff.

--- NESCore/NES.cs
+++ NESCore/NES.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Drawing;
 using System.IO;
+using System.Threading;
 using NESCore.Mappers;
 using Serilog;
 
@@ -12,8 +13,8 @@ namespace NESCore
         
         public readonly Bus Bus;
         public bool Running;
-        private int lastSecond;
         private int uptimeSeconds;
+        private int nanoPerCycle;
 
         public NES(int speedInHz = 1789773)
         {
@@ -24,6 +25,8 @@ namespace NESCore
 
             Bus.Cpu.PowerUp();
             ConfigureLogger();
+
+            nanoPerCycle = (int) ((1.0 / speedInHz) * 1000000000);
 
             Bus.Ppu.OnNewFrame += (ref int[] frame) => OnNewFrame?.Invoke(ref frame);
         }
@@ -59,24 +62,47 @@ namespace NESCore
 
         public void Step()
         {
-            if(Bus.Cpu.cyclesThisSec % 10000 == 1)
-            {
-                var s = DateTime.Now.Second;
-                if (s != lastSecond)
-                {
-                    lastSecond = s;
-                    uptimeSeconds++;
-                    Log.Information($"Second: {uptimeSeconds} FPS: {Bus.Ppu.FrameCount}");
-                    //Console.WriteLine($"Second: {uptimeSeconds} FPS: {Bus.Ppu.FrameCount}");
-                    Bus.Ppu.FrameCount = 0;
-                    Bus.Cpu.cyclesThisSec = 0;
-                }
-            }
-
+            MatchSpeed();
             var cpuCycles = Bus.Cpu.Instruction();
             Bus.Ppu.RunCycles(cpuCycles*3);
         }
 
+        
+        private int timer = 0;
+        private DateTime lastSecond;
+        private double cpuTimer;
+        private void MatchSpeed()
+        {
+            //Don't try to match the speed all the time, it'll kill the CPU doing all the time calculations
+            if (Bus.Cpu.cyclesThisSec % 10000 != 1)
+            {
+                return;
+            }
+            
+            //A second has passed so start over
+            if (cpuTimer > 1000)
+            {
+                Console.WriteLine($"Second: {uptimeSeconds} FPS: {Bus.Ppu.FrameCount}");
+                cpuTimer = 0;
+                Bus.Ppu.FrameCount = 0;
+                Bus.Cpu.cyclesThisSec = 0;
+                lastSecond = DateTime.Now;
+                uptimeSeconds++;
+            }
+
+            //In milliseconds
+            var shouldHaveElapsed = (Bus.Cpu.cyclesThisSec * nanoPerCycle)/1000000;
+            
+            //The PC is going faster than the emulated cpu. Wait!
+            if (shouldHaveElapsed > cpuTimer)
+            {
+                //Console.WriteLine($"Sleeping for {shouldHaveElapsed - cpuTimer}");
+                Thread.Sleep((int)(shouldHaveElapsed - cpuTimer));
+            }
+
+            cpuTimer = (DateTime.Now - lastSecond).TotalMilliseconds;
+        }
+        
         public void Stop()
         {
             Running = false;
